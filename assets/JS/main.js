13bb9a224b0fb48a3a71fe27ae2a15484721e2a7
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-02-25 13:50:20 +0100

    add card markup with api ajax call

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,12 +1,24 @@
 //link api 
 const endpoint = 'https://lanciweb.github.io/demo/api/pictures/'
+const column = document.getElementsByClassName('col')[0] 
 fetch(endpoint)
 .then(res => res.json())
 .then (data =>{
     console.log(data);
-    data.forEach(cardObj => {
-    let card = cardObj
     
+    data.forEach(cardObj => {
+        let card = cardObj
+        console.log(card);
+        
+        const markup =`
+        <div class="card">
+        <img src="./assets/img/pin.svg" alt="pin" class="pin">
+            <img class="card-img" src='${card.url}'></img>
+            <div class="card-title">${card.title}</div>
+            <div class="card-date">${card.date}</div>
+        </div> 
+        `
+        column.innerHTML += markup
     });
     
 
