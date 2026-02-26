45f74c57db554b4f3c3d7beeeb50a34bb4bf51d8
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-02-26 11:02:38 +0100

    add all markup  instantly

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,31 +1,25 @@
 //link api 
-const endpoint = 'https://lanciweb.github.io/demo/api/pictures/'
-const column = document.getElementsByClassName('row')[0] 
-const closeBtn = document.getElementById('close-btn')
+const endpoint = 'https://lanciweb.github.io/demo/api/pictures/';
+const column = document.getElementsByClassName('row')[0]; 
+const closeBtn = document.getElementById('close-btn');
+const imageEl = document.getElementsByClassName("image");
 fetch(endpoint)
 .then(res => res.json())
-.then (data =>{
-    console.log(data);
-    
-    data.forEach(cardObj => {
-        let card = cardObj
-        console.log(card);
-        
-        const markup =`
-        <div class="card">
+.then(data => {
+
+let cardsHTML = "";
+data.forEach(card => {
+    cardsHTML += `
+    <div class="card">
         <img src="./assets/img/pin.svg" alt="pin" class="pin">
-            <img class="card-img" src='${card.url}'></img>
-            <div class="card-date">${card.date}</div>
-            <div class="card-title">${card.title.toUpperCase()}</div>
-        </div> 
-        `
-        column.innerHTML += markup
-
-    });
-    
-
-
+        <img class="card-img" src="${card.url}" alt="${card.title}">
+        <div class="card-date">${card.date}</div>
+        <div class="card-title">${card.title.toUpperCase()}</div>
+    </div>`;
+});
+column.innerHTML = cardsHTML;
 })
+    
 off()
 function on() {
   document.getElementById("overlay").style.display = "block";
@@ -34,3 +28,4 @@ function off() {
   document.getElementById("overlay").style.display = "none";
 }
 closeBtn.addEventListener('click', off)
+
